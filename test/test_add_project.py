a13cf72cebcdca02c0b12d1b484cd27541bd3af5
Olya Chechenina <checheninao@gmail.com>
2017-01-09 23:06:51 +0300

    задание26: проверка списков проектов реализована через SOAP

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -5,7 +5,7 @@ def test_add_project(app, json_projects):
     old_projects = app.soap.get_project_list()
     project = json_projects
     app.project.create(project)
-    new_projects = app.soap.project.get_project_list()
+    new_projects = app.soap.get_project_list()
     if project.name != "" and project.name not in [proj.name for proj in old_projects]:
        old_projects.append(project)
     assert len(old_projects) == len(new_projects)
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -5,10 +5,10 @@ import random
 def test_delete_project(app):
     if app.project.count() == 0:
         app.project.create(Project(name="proj1"))
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_list()
     project = random.choice(old_projects)
     app.project.delete_project_by_name(project.name)
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_project_list()
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.key) == sorted(new_projects, key=Project.key)
